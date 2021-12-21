195ee59a988dee7288de9fe22ad69c30bca8c909
Nikolas Andryuschenko <nikolaigeorgie@gmail.com>
2021-12-20 23:14:42 -0800

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+const cors = require('cors')
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors())
   await app.listen(3000);
 }
 bootstrap();
